33921f0a95f044c2071bfa7c393e845fce4beef3
Itamar Joire <ijoire7@gmail.com>
2024-05-22 22:31:09 -0300

    mask:linear-gradient

--- src/components/ui/services.tsx
+++ src/components/ui/services.tsx
@@ -9,6 +9,7 @@ export function Services(){
 
         <div className="grid grid-cols-1 mt-10 text-center sm:mt-16 sm:grid-cols-2 sm:gap-x-12 gap-y-12 md:grid-cols-2 md:gap-0 xl:mt-24">
             <div className="md:p-8 lg:p-14 ">
+            <div className="h-full p-7 mx-auto max-h-[110px] w-11/12 max-w-[200px] rounded-lg  bg-zinc-900/35 [mask:linear-gradient(black,black_75%,transparent)] md:w-4/5">
                 <svg className="mx-auto" width="52" height="52" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g filter="url(#filter0_f_240_81)">
 <path d="M23.0004 29C26.3141 29 29.0004 26.3137 29.0004 23C29.0004 19.6863 26.3141 17 23.0004 17C19.6867 17 17.0004 19.6863 17.0004 23C17.0004 26.3137 19.6867 29 23.0004 29Z" stroke="url(#paint0_linear_240_81)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
@@ -40,61 +41,63 @@ export function Services(){
 </linearGradient>
 </defs>
                 </svg>
-
+            </div>
                 <h3 className="mt-6 text-xl sm:text-2xl font-bold text-white font-pj">Redes sociais</h3>
                 <p className="mt-5 text-base text-gray-400 sm:text-lg">Gerenciamento, planejamento, criação de artes, conteúdo estratégico, interação com seguidores, gestão e análise de métricas.</p>
             </div>
 
-            <div className="md:p-8 lg:p-14 md:border-l md:border-gray-400/25">       
-                <svg className="mx-auto" width="52" height="52" viewBox="0 0 48 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g clip-path="url(#clip0_241_92)" filter="url(#filter0_f_241_92)">
-    <path d="M7.5 14V38L18 32L30 38L40.5 32V8L30 14L18 8L7.5 14Z" stroke="url(#paint0_linear_241_92)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M18 8V32" stroke="url(#paint1_linear_241_92)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M30 14V38" stroke="url(#paint2_linear_241_92)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
-    </g>
-    <g clip-path="url(#clip1_241_92)">
-    <path d="M7.5 14V38L18 32L30 38L40.5 32V8L30 14L18 8L7.5 14Z" stroke="url(#paint3_linear_241_92)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M18 8V32" stroke="url(#paint4_linear_241_92)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M30 14V38" stroke="url(#paint5_linear_241_92)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
-    </g>
-    <defs>
-    <filter id="filter0_f_241_92" x="0" y="-1" width="48" height="48" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-    <feGaussianBlur stdDeviation="3" result="effect1_foregroundBlur_241_92"/>
-    </filter>
-    <linearGradient id="paint0_linear_241_92" x1="38.1429" y1="10.7273" x2="13.145" y2="38.438" gradientUnits="userSpaceOnUse">
-    <stop stop-color="#FF7205"/>
-    <stop offset="1" stop-color="#FFA122"/>
-    </linearGradient>
-    <linearGradient id="paint1_linear_241_92" x1="18.9286" y1="10.1818" x2="17.2432" y2="10.2526" gradientUnits="userSpaceOnUse">
-    <stop stop-color="#FF7205"/>
-    <stop offset="1" stop-color="#FFA122"/>
-    </linearGradient>
-    <linearGradient id="paint2_linear_241_92" x1="30.9286" y1="16.1818" x2="29.2432" y2="16.2526" gradientUnits="userSpaceOnUse">
-    <stop stop-color="#FF7205"/>
-    <stop offset="1" stop-color="#FFA122"/>
-    </linearGradient>
-    <linearGradient id="paint3_linear_241_92" x1="38.1429" y1="10.7273" x2="13.145" y2="38.438" gradientUnits="userSpaceOnUse">
-    <stop stop-color="#FF7205"/>
-    <stop offset="1" stop-color="#FFA122"/>
-    </linearGradient>
-    <linearGradient id="paint4_linear_241_92" x1="18.9286" y1="10.1818" x2="17.2432" y2="10.2526" gradientUnits="userSpaceOnUse">
-    <stop stop-color="#FF7205"/>
-    <stop offset="1" stop-color="#FFA122"/>
-    </linearGradient>
-    <linearGradient id="paint5_linear_241_92" x1="30.9286" y1="16.1818" x2="29.2432" y2="16.2526" gradientUnits="userSpaceOnUse">
-    <stop stop-color="#FF7205"/>
-    <stop offset="1" stop-color="#FFA122"/>
-    </linearGradient>
-    <clipPath id="clip0_241_92">
-    <rect width="36" height="36" fill="white" transform="translate(6 5)"/>
-    </clipPath>
-    <clipPath id="clip1_241_92">
-    <rect width="36" height="36" fill="white" transform="translate(6 5)"/>
-    </clipPath>
-    </defs>
-                </svg>
+            <div className="md:p-8 lg:p-14 md:border-l md:border-gray-400/25">    
+                <div className="h-full p-7 mx-auto max-h-[110px] w-11/12 max-w-[200px] rounded-lg  bg-zinc-900/35 [mask:linear-gradient(black,black_75%,transparent)] md:w-4/5"> 
+                    <svg className="mx-auto" width="52" height="52" viewBox="0 0 48 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_241_92)" filter="url(#filter0_f_241_92)">
+        <path d="M7.5 14V38L18 32L30 38L40.5 32V8L30 14L18 8L7.5 14Z" stroke="url(#paint0_linear_241_92)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M18 8V32" stroke="url(#paint1_linear_241_92)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M30 14V38" stroke="url(#paint2_linear_241_92)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
+        </g>
+        <g clip-path="url(#clip1_241_92)">
+        <path d="M7.5 14V38L18 32L30 38L40.5 32V8L30 14L18 8L7.5 14Z" stroke="url(#paint3_linear_241_92)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M18 8V32" stroke="url(#paint4_linear_241_92)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M30 14V38" stroke="url(#paint5_linear_241_92)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
+        </g>
+        <defs>
+        <filter id="filter0_f_241_92" x="0" y="-1" width="48" height="48" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+        <feGaussianBlur stdDeviation="3" result="effect1_foregroundBlur_241_92"/>
+        </filter>
+        <linearGradient id="paint0_linear_241_92" x1="38.1429" y1="10.7273" x2="13.145" y2="38.438" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#FF7205"/>
+        <stop offset="1" stop-color="#FFA122"/>
+        </linearGradient>
+        <linearGradient id="paint1_linear_241_92" x1="18.9286" y1="10.1818" x2="17.2432" y2="10.2526" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#FF7205"/>
+        <stop offset="1" stop-color="#FFA122"/>
+        </linearGradient>
+        <linearGradient id="paint2_linear_241_92" x1="30.9286" y1="16.1818" x2="29.2432" y2="16.2526" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#FF7205"/>
+        <stop offset="1" stop-color="#FFA122"/>
+        </linearGradient>
+        <linearGradient id="paint3_linear_241_92" x1="38.1429" y1="10.7273" x2="13.145" y2="38.438" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#FF7205"/>
+        <stop offset="1" stop-color="#FFA122"/>
+        </linearGradient>
+        <linearGradient id="paint4_linear_241_92" x1="18.9286" y1="10.1818" x2="17.2432" y2="10.2526" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#FF7205"/>
+        <stop offset="1" stop-color="#FFA122"/>
+        </linearGradient>
+        <linearGradient id="paint5_linear_241_92" x1="30.9286" y1="16.1818" x2="29.2432" y2="16.2526" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#FF7205"/>
+        <stop offset="1" stop-color="#FFA122"/>
+        </linearGradient>
+        <clipPath id="clip0_241_92">
+        <rect width="36" height="36" fill="white" transform="translate(6 5)"/>
+        </clipPath>
+        <clipPath id="clip1_241_92">
+        <rect width="36" height="36" fill="white" transform="translate(6 5)"/>
+        </clipPath>
+        </defs>
+                    </svg>
+                </div>  
 
                 <h3 className="mt-6 text-xl sm:text-2xl font-bold text-white font-pj">Tráfego pago</h3>
                 <p className="mt-5 text-base text-gray-400 sm:text-lg">Criação de campanhas nas plataformas de anúncios, otimização e análise de resultados.</p>
@@ -102,7 +105,7 @@ export function Services(){
 
             <div className="md:p-8 lg:p-14 md:border-t md:border-gray-400/25">
     
-              
+            <div className="h-full p-7 mx-auto max-h-[110px] w-11/12 max-w-[200px] rounded-lg  bg-zinc-900/35 [mask:linear-gradient(black,black_75%,transparent)] md:w-4/5">
                 <svg className="mx-auto" width="52" height="52" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g filter="url(#filter0_f_241_103)">
 <path d="M23.0002 33.5L33.5002 23L38.0002 27.5L27.5002 38L23.0002 33.5Z" stroke="url(#paint0_linear_241_103)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
@@ -154,7 +157,7 @@ export function Services(){
 </linearGradient>
 </defs>
                 </svg>
-
+            </div>
 
 
                 <h3 className="mt-6 text-xl sm:text-2xl font-bold text-white font-pj">Design</h3>
@@ -162,6 +165,7 @@ export function Services(){
             </div>
 
             <div className="md:p-8 lg:p-14 md:border-t md:border-l md:border-gray-400/25"> 
+            <div className="h-full p-7 mx-auto max-h-[110px] w-11/12 max-w-[200px] rounded-lg  bg-zinc-900/35 [mask:linear-gradient(black,black_75%,transparent)] md:w-4/5">
                 <svg className="mx-auto" width="52" height="52" viewBox="0 0 46 42" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g filter="url(#filter0_f_240_74)">
                     <path d="M35.0004 7.5H11.0004C9.34354 7.5 8.0004 8.84315 8.0004 10.5V25.5C8.0004 27.1569 9.34354 28.5 11.0004 28.5H35.0004C36.6573 28.5 38.0004 27.1569 38.0004 25.5V10.5C38.0004 8.84315 36.6573 7.5 35.0004 7.5Z" stroke="url(#paint0_linear_240_74)" stroke-width="3.00312" stroke-linecap="round" stroke-linejoin="round"/>
@@ -203,7 +207,7 @@ export function Services(){
                     </linearGradient>
                     </defs>
                 </svg>
-
+            </div>
                 <h3 className="mt-6 text-xl sm:text-2xl font-bold text-white font-pj">Sites</h3>
                 <p className="mt-5 text-base text-gray-400 sm:text-lg">Criação de layout personalizado, design responsivo, integração com as redes sociais e com o WhatsApp.</p>
             </div>
